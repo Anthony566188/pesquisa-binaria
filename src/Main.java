9ac6033a8316c86c38fcea849a5c5f391f59a311
Anthony566188 <souzaanthony892@gmail.com>
2025-10-25 12:36:27 -0300

    atualização

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         int[] lista = {1, 3, 5, 7, 9};
 
         Pesquisa pesquisa = new Pesquisa();
 
-        System.out.println(pesquisa.pesquisa_binaria(lista, 3));
-        System.out.println(pesquisa.pesquisa_binaria(lista, -1));
+        System.out.println(pesquisa.pesquisaBinaria(lista, 3));
+        System.out.println(pesquisa.pesquisaBinaria(lista, -1));
 
 
     }
